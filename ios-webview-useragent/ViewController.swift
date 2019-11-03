5d9f56582fda95274e52d61e6b17f34d0eeb07ca
t nakata <git.nakata0615@gmail.com>
2019-11-03 17:32:23 +0900

    ユーザーエージェントに独自の文字列追加

--- ios-webview-useragent/ViewController.swift
+++ ios-webview-useragent/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupWebView()
     }
 
+    private func setupWebView() {
+        let config = WKWebViewConfiguration()
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        config.applicationNameForUserAgent = "sample/\(version)"
 
+        self.webView = WKWebView(frame: self.containerView.bounds, configuration: config)
+        self.webView.navigationDelegate = self
+        self.webView.uiDelegate = self
+        self.webView.allowsBackForwardNavigationGestures = true
+        self.webView.scrollView.bounces = true
+        self.containerView.addSubview(self.webView)
+        let urlRequest = URLRequest(url: URL(string: "https://www.google.com/")!)
+        self.webView.load(urlRequest)
+    }
 }
 
+extension ViewController: WKNavigationDelegate {
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    }
+}
+
+extension ViewController: WKUIDelegate {
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration,
+                 for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        return nil
+    }
+}
