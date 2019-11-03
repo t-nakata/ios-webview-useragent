4d6829363dc452cbbb081ad18aac076987a0e9dd
t nakata <git.nakata0615@gmail.com>
2019-11-03 13:37:26 +0900

    initial commit

---
+++ ios-webview-useragent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-webview-useragent
+//
+//  Created by なかた(@nkt0615) on 2019/11/03.
+//  Copyright © 2019 なかた(@nkt0615). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
